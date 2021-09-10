2a2a247dfa8f7e0cadbb89110d4d82f9b971c7ed
Solitary <zaryabqayyum2@gmail.com>
2021-09-10 16:52:51 +0500

    Little Change

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
-import 'package:flutter/services.dart';
 
 class CityScreen extends StatefulWidget {
   @override
